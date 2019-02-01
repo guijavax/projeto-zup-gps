7ce74d656cd4dabc13ecc08ad1b011317fd3a256
Guilherme <guiborgesTi>
2019-02-01 00:36:40 -0200

    estruturando o projeto

---
+++ src/main/java/com/example/demo/ProjetoZupGpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoZupGpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoZupGpsApplication.class, args);
+	}
+
+}
+
